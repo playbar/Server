a8fd9e13aab5eb68cb05acb23745f76d7f7468a5
hgl888 <hgl868@126.com>
2016-08-04 20:53:11 +0800

    modify code

---
+++ base/Base64.cpp
@@ -0,0 +1,9 @@
+//
+//  Base64.cpp
+//  Server
+//
+//  Created by mac on 16/8/4.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#include "Base64.h"
---
+++ base/Base64.h
@@ -0,0 +1,14 @@
+//
+//  Base64.hpp
+//  Server
+//
+//  Created by mac on 16/8/4.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#ifndef __Base64_h__
+#define __Base64_h__
+
+#include <stdio.h>
+
+#endif /* Base64_hpp */
