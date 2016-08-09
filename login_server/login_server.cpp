5b37b8d25d02ebfd70e180462ebf6f337c6de870
hgl888 <hgl868@126.com>
2016-08-09 18:29:07 +0800

    modify code

---
+++ login_server/login_server.cpp
@@ -0,0 +1,15 @@
+//
+//  login_server.cpp
+//  Server
+//
+//  Created by mac on 16/8/9.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main( int argc, char *argv[]){
+    int i = 0;
+    printf("server version:%d", i);
+    return 0;
+}
