c600190ba49053dab25b91da916e4ad04f35e56d
hgl888 <hgl868@126.com>
2016-08-26 14:52:22 +0800

    add function

--- base/BaseSocket.cpp
+++ base/BaseSocket.cpp
@@ -35,6 +35,59 @@ CBaseSocket *FindBaseSocket( net_handle_t fd )
 
 //////////////
 
+CBaseSocket::CBaseSocket()
+{
+    m_socket = INVALID_SOCKET;
+    m_state = SOCKET_STATE_IDLE;
+}
+
+CBaseSocket::~CBaseSocket()
+{
+    
+}
+int CBaseSocket::Listen(const char *server_ip, uint16_t port, callback_t callback, void *callback_data)
+{
+    m_local_ip = server_ip;
+    m_local_port = port;
+    m_callback = callback;
+    m_callback_data = callback_data;
+    
+    m_socket = socket( AF_INET, SOCK_STREAM, 0 );
+    if( m_socket == INVALID_SOCKET )
+    {
+        printf("socket failed, err_code=%d\n", _GetErrorCode());
+        return NETLIB_ERROR;
+    }
+    _SetReuseAddr(m_socket);
+    _SetNonblock(m_socket);
+    
+    sockaddr_in serv_addr;
+    _SetAddr( server_ip, port, &serv_addr );
+    int ret = ::bind(m_socket, (sockaddr*)&serv_addr, sizeof( serv_addr));
+    if( ret == SOCKET_ERROR)
+    {
+        log("bind failed, err_code=%d", _GetErrorCode());
+        closesocket( m_socket);
+        return NETLIB_ERROR;
+    }
+    
+    ret = listen(m_socket, 64);
+    if( ret == SOCKET_ERROR )
+    {
+        log("listen failed, err_code=%d", _GetErrorCode() );
+        closesocket(m_socket);
+        return NETLIB_ERROR;
+    }
+    
+    m_state = SOCKET_STATE_LISTENING;
+    
+    log("CBaseSocket::Listen on %s:%d", server_ip, port);
+    
+    AddBaseSocket(this);
+    CEventDispatch::Instance()->AddEvent(m_socket, SOCKET_READ | SOCKET_EXCEP);
+    return NETLIB_OK;
+    
+}
 
 
 
