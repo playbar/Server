f764470ebaabbd641274464787e926f6f3ae942b
hgl888 <hgl868@126.com>
2016-09-02 17:36:37 +0800

    add pb files

---
+++ base/HttpClient.cpp
@@ -0,0 +1,16 @@
+//
+//  HttpClient.cpp
+//  Server
+//
+//  Created by mac on 16/9/2.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#include "HttpClient.h"
+#include <curl/curl.h>
+#include "public_define.h"
+
+class CHttpClient
+{
+    
+};
---
+++ base/HttpClient.h
@@ -0,0 +1,14 @@
+//
+//  HttpClient.hpp
+//  Server
+//
+//  Created by mac on 16/9/2.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#ifndef __HTTP_CURL_H__
+#define __HTTP_CURL_H__
+
+#include <stdio.h>
+
+#endif /* HttpClient_h */
