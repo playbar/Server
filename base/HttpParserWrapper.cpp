8374f93809ad2e03f5b532c16bef7df17bf746f8
hgl888 <hgl868@126.com>
2016-09-07 08:39:11 +0800

    add function

--- base/HttpParserWrapper.cpp
+++ base/HttpParserWrapper.cpp
@@ -48,3 +48,124 @@ void CHttpParserWrapper::ParserHttpContent(const char *buf, uint32_t len)
     return;
     
 }
+
+int CHttpParserWrapper::OnUrl(http_parser *parser, const char *at, size_t length, void *obj)
+{
+    ((CHttpParserWrapper*)obj)->SetUrl(at, length);
+    return 0;
+}
+
+int CHttpParserWrapper::OnHeaderField(http_parser *parser, const char *at, size_t length, void *obj)
+{
+    if(!((CHttpParserWrapper*)obj)->HasReadReferer())
+    {
+        if( strncasecmp(at, "Referer", 7) == 0)
+        {
+            ((CHttpParserWrapper*)obj)->SetReadReferer(true);
+        }
+    }
+    
+    if( !((CHttpParserWrapper*)obj)->HasReadForwardIP())
+    {
+        if( strncasecmp(at, "X-Forwarded-For", 15) == 0)
+        {
+            ((CHttpParserWrapper*)obj)->SetReadForwardIP(true);
+        }
+    }
+    
+    if( !((CHttpParserWrapper*)obj)->HasReadUserAgent())
+    {
+        if( strncasecmp(at, "User-Agent", 10) == 0)
+        {
+            ((CHttpParserWrapper*)obj)->SetReadUserAgent(true);
+        }
+    }
+    
+    if (!((CHttpParserWrapper*)obj)->HasReadContentType())
+    {
+        if (strncasecmp(at, "Content-Type", 12) == 0)
+        {
+            ((CHttpParserWrapper*)obj)->SetReadContentType(true);
+        }
+    }
+    
+    if(!((CHttpParserWrapper*)obj)->HasReadContentLen())
+    {
+        if(strncasecmp(at, "Content-Length", 14) == 0)
+        {
+            ((CHttpParserWrapper*)obj)->SetReadContentLen(true);
+        }
+    }
+    if(!((CHttpParserWrapper*)obj)->HasReadHost())
+    {
+        if(strncasecmp(at, "Host", 4) == 0)
+        {
+            ((CHttpParserWrapper*)obj)->SetReadHost(true);
+        }
+    }
+    
+    return 0;
+}
+
+
+int CHttpParserWrapper::OnHeaderValue(http_parser* parser, const char *at, size_t length, void* obj)
+{
+    if (((CHttpParserWrapper*)obj)->IsReadReferer())
+    {
+        size_t referer_len =
+        (length > MAX_REFERER_LEN) ? MAX_REFERER_LEN : length;
+        ((CHttpParserWrapper*)obj)->SetReferer(at, referer_len);
+        ((CHttpParserWrapper*)obj)->SetReadReferer(false);
+    }
+    
+    if (((CHttpParserWrapper*)obj)->IsReadForwardIP())
+    {
+        ((CHttpParserWrapper*)obj)->SetForwardIP(at, length);
+        ((CHttpParserWrapper*)obj)->SetReadForwardIP(false);
+    }
+    
+    if (((CHttpParserWrapper*)obj)->IsReadUserAgent())
+    {
+        ((CHttpParserWrapper*)obj)->SetUserAgent(at, length);
+        ((CHttpParserWrapper*)obj)->SetReadUserAgent(false);
+    }
+    
+    if (((CHttpParserWrapper*)obj)->IsReadContentType())
+    {
+        ((CHttpParserWrapper*)obj)->SetContentType(at, length);
+        ((CHttpParserWrapper*)obj)->SetReadContentType(false);
+    }
+    
+    if(((CHttpParserWrapper*)obj)->IsReadContentLen())
+    {
+        string strContentLen(at, length);
+        ((CHttpParserWrapper*)obj)->SetContentLen(atoi(strContentLen.c_str()));
+        ((CHttpParserWrapper*)obj)->SetReadContentLen(false);
+    }
+    
+    if(((CHttpParserWrapper*)obj)->IsReadHost())
+    {
+        ((CHttpParserWrapper*)obj)->SetHost(at, length);
+        ((CHttpParserWrapper*)obj)->SetReadHost(false);
+    }
+    return 0;
+}
+
+int CHttpParserWrapper::OnHeadersComplete(http_parser* parser, void* obj)
+{
+    ((CHttpParserWrapper*)obj)->SetTotalLength(parser->nread + (uint32_t) parser->content_length);
+    return 0;
+}
+
+int CHttpParserWrapper::OnBody(http_parser* parser, const char *at, size_t length, void* obj)
+{
+    ((CHttpParserWrapper*)obj)->SetBodyContent(at, length);
+    return 0;
+}
+
+int CHttpParserWrapper::OnMessageComplete(http_parser* parser, void* obj)
+{
+    ((CHttpParserWrapper*)obj)->SetReadAll();
+    return 0;
+}
+
