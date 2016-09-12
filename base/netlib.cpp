7615d79f8dca8d8a6882e8f7aa66b0d77c6193d2
hgl888 <hgl868@126.com>
2016-09-12 08:51:18 +0800

    add fun

--- base/netlib.cpp
+++ base/netlib.cpp
@@ -38,4 +38,47 @@ int netlib_listen(const char *server_ip, uint16_t port,
     return ret;
 }
 
+net_handle_t netlib_connect(const char *server_ip, uint16_t port,
+                            callback_t callback, void *callback_data)
+{
+    CBaseSocket *pSocket = new CBaseSocket();
+    if( NULL == pSocket )
+    {
+        return NETLIB_INVALID_HANDLE;
+    }
+    
+    net_handle_t handle = pSocket->Connect(server_ip, port, callback, callback_data);
+    if( handle == NETLIB_INVALID_HANDLE)
+    {
+        delete pSocket;
+    }
+    return handle;
+}
+
+int netlib_send( net_handle_t handle, void *buf, int len)
+{
+    CBaseSocket *pSocket = FindBaseSocket(handle);
+    if( NULL == pSocket)
+    {
+        return NETLIB_ERROR;
+    }
+    int ret = pSocket->Send(buf, len);
+    pSocket->ReleaseRef();
+    return ret;
+}
+
+int netlib_recv( net_handle_t handle, void *buf, int len)
+{
+    CBaseSocket *pSocket = FindBaseSocket(handle);
+    if( NULL == pSocket)
+    {
+        return NETLIB_ERROR;
+    }
+    int ret = pSocket->Recv(buf, len);
+    pSocket->ReleaseRef();
+    return ret;
+}
+
+
+
 
