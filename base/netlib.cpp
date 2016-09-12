e3ba85f90ec6c8928094c01199c24e250ff0b00f
hgl888 <hgl868@126.com>
2016-09-12 09:12:01 +0800

    修改代码

--- base/netlib.cpp
+++ base/netlib.cpp
@@ -79,6 +79,95 @@ int netlib_recv( net_handle_t handle, void *buf, int len)
     return ret;
 }
 
+int netlib_close( net_handle_t handle)
+{
+    CBaseSocket *pSocket = FindBaseSocket(handle);
+    if( NULL == pSocket)
+    {
+        return NETLIB_ERROR;
+    }
+    int ret = pSocket->Close();
+    pSocket->ReleaseRef();
+    return ret;
+}
+
+int netlib_option( net_handle_t handel, int opt, void *optval)
+{
+    CBaseSocket *pSocket = FindBaseSocket(handel);
+    if( NULL == pSocket )
+    {
+        return NETLIB_ERROR;
+    }
+    
+    if((opt >= NETLIB_OPT_GET_REMOTE_IP) && !optval )
+    {
+        return NETLIB_ERROR;
+    }
+    
+    switch (opt) {
+        case NETLIB_OPT_SET_CALLBACK:
+            pSocket->SetCallback((callback_t)optval);
+            break;
+        case NETLIB_OPT_SET_CALLBACK_DATA:
+            pSocket->SetCallbackData(optval);
+            break;
+        case NETLIB_OPT_GET_REMOTE_IP:
+            *(string*)optval = pSocket->GetRemoteIP();
+            break;
+        case NETLIB_OPT_GET_REMOTE_PROT:
+            *(string*)optval = pSocket->GetRemotePort();
+            break;
+        case NETLIB_OPT_GET_LOCAL_IP:
+            *(string*)optval = pSocket->GetLocalIP();
+            break;
+        case NETLIB_OPT_GET_LOCAL_PORT:
+            *(string*)optval = pSocket->GetLocalPort();
+            break;
+        case NETLIB_OPT_SET_SEND_BUF_SIZE:
+            pSocket->SetSendBufSize(*(uint32_t*)optval);
+            break;
+        case NETLIB_OPT_SET_RECV_BUF_SIZE:
+            pSocket->SetRecvBufSize(*(uint32_t*)optval);
+            break;
+    }
+    pSocket->ReleaseRef();
+    return NETLIB_OK;
+}
+
+int netlib_register_timer( callback_t callback, void *user_data, uint64_t interval)
+{
+    CEventDispatch::Instance()->AddTimer(callback, user_data, interval);
+    return 0;
+}
+
+int netlib_delete_timer( callback_t callback, void *user_data)
+{
+    CEventDispatch::Instance()->RemoveTimer(callback, user_data);
+    return 0;
+}
+
+int netlib_add_loop(callback_t callback, void *user_data)
+{
+    CEventDispatch::Instance()->AddLoop(callback, user_data);
+    return 0;
+}
+
+void netlib_eventloop(uint32_t wait_timeout)
+{
+    CEventDispatch::Instance()->StartDispatch(wait_timeout);
+    return;
+}
+
+void netlib_stop_event()
+{
+    CEventDispatch::Instance()->StopDispatch();
+    return;
+}
+
+bool netlib_is_runnig()
+{
+    return CEventDispatch::Instance()->isRunning();
+}
 
 
 
