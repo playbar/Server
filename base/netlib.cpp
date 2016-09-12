00e9e54310d0ff63997a6d4774ff7cb5914deb98
hgl888 <hgl868@126.com>
2016-09-12 08:43:11 +0800

    添加函数

--- base/netlib.cpp
+++ base/netlib.cpp
@@ -7,3 +7,35 @@
 //
 
 #include "netlib.h"
+#include "BaseSocket.h"
+#include "EventDispatch.h"
+
+int netlib_init()
+{
+    int ret = NETLIB_OK;
+    return ret;
+}
+
+
+int netlib_destroy()
+{
+    int ret = NETLIB_OK;
+    return ret;
+}
+
+int netlib_listen(const char *server_ip, uint16_t port,
+                  callback_t callback, void *callback_data)
+{
+    CBaseSocket *pSocket = new CBaseSocket();
+    if( NULL == pSocket )
+    {
+        return NETLIB_ERROR;
+    }
+    int ret = pSocket->Listen(server_ip, port, callback, callback_data);
+    if( ret == NETLIB_ERROR){
+        delete pSocket;
+    }
+    return ret;
+}
+
+
