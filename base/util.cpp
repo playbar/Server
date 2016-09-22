11fdf0723afcac18f178fd1653598ffa0a6526d9
hgl888 <hgl868@126.com>
2016-09-22 11:22:02 +0800

    login_server

--- base/util.cpp
+++ base/util.cpp
@@ -250,3 +250,55 @@ int64_t get_file_size(const char *path)
     return filesize;
 }
 
+const char *memfind(const char *src_str, size_t src_len, const char *sub_str, size_t sub_len, bool flag)
+{
+    if( NULL == src_str || NULL == sub_str || src_len <= 0)
+    {
+        return NULL;
+    }
+    if( src_len < sub_len)
+    {
+        return NULL;
+    }
+    const char *p;
+    if( sub_len == 0 )
+    {
+        sub_len = strlen(sub_str);
+    }
+    if( src_len == sub_len)
+    {
+        if( 0 == (memcmp(src_str, sub_str, src_len)))
+        {
+            return src_str;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+    if( flag)
+    {
+        for( int i = 0; i < src_len - sub_len; ++i)
+        {
+            p = src_str + i;
+            if( 0 == memcmp(p, sub_str, sub_len))
+            {
+                return p;
+            }
+        }
+    }
+    else
+    {
+        for( int i = (src_len - sub_len); i >= 0; --i )
+        {
+            p = src_str + i;
+            if( 0 == memcmp(p, sub_str, sub_len))
+            {
+                return p;
+            }
+        }
+    }
+    return NULL;
+}
+
+
--- login_server/login_server.cpp
+++ login_server/login_server.cpp
@@ -23,8 +23,43 @@ void client_callback(void *callback_data, uint8_t msg, uint32_t handle, void *pP
     if( NETLIB_MSG_CONNECT == msg )
     {
         CLoginConn *pConn = new CLoginConn();
+        pConn->OnConnect2(handle, LOGIN_CONN_TYPE_CLIENT);
+    }
+    else
+    {
+        log("!!!error msg: %d ", msg);
     }
 }
+
+void msg_serv_callback(void *callback_data, uint8_t msg, uint32_t handle, void *pParam)
+{
+    log("msg_server come in");
+    
+    if( msg == NETLIB_MSG_CONNECT )
+    {
+        CLoginConn *pConn = new CLoginConn();
+        pConn->OnConnect2(handle, LOGIN_CONN_TYPE_MSG_SERV);
+    }
+    else
+    {
+        log("!!!error msg: %d ", msg);
+    }
+}
+
+void http_callback(void *callback_data, uint8_t msg, uint32_t handle, void *pParam)
+{
+    if( NETLIB_MSG_CONNECT == msg )
+    {
+        CHttpConn *pConn = new CHttpConn();
+        pConn->OnConnect(handle);
+    }
+    else
+    {
+        log("!!!error msg: %d ", msg);
+    }
+}
+
+
 
 void test()
 {
@@ -47,6 +82,79 @@ void test()
 
 int main( int argc, char *argv[]){
     test();
+    if((argc == 2) &&(strcmp(argv[1], "-v") ==0))
+    {
+        printf("Server Version: LoginServer/%s\n", VERSION);
+        printf("Server Build: %s %s\n", __DATE__, __TIME__);
+    }
+    
+    signal(SIGPIPE, SIG_IGN);
+    
+    CConfigFileReader config_file("loginserver.conf");
+    
+    char *client_listen_ip = config_file.GetConfigName("ClientListenIP");
+    char *str_client_port = config_file.GetConfigName("ClientPort");
+    char *http_listen_ip = config_file.GetConfigName("HttpListenIP");
+    char *str_http_port = config_file.GetConfigName("HttpPort");
+    char *msg_server_listen_ip = config_file.GetConfigName("MsgServerListenIP");
+    char *str_msg_server_port = config_file.GetConfigName("MsgServerPort");
+    char *str_msfs_url = config_file.GetConfigName("msfs");
+    char *str_discovery = config_file.GetConfigName("discovery");
+    
+    if( !msg_server_listen_ip || !str_msg_server_port || !http_listen_ip
+       || !str_http_port || !str_msfs_url || !str_discovery )
+    {
+        log("config item missing, exit... ");
+        return -1;
+    }
+    
+    uint16_t client_port = atoi(str_client_port);
+    uint16_t msg_server_port = atoi(str_msg_server_port);
+    uint16_t http_port = atoi(str_http_port);
+    strMsfsUrl = str_msfs_url;
+    strDiscovery = str_discovery;
+    
+    pIpParser = new IpParser();
+    int ret = netlib_init();
+    if( NETLIB_ERROR == ret )
+    {
+        return ret;
+    }
+    
+    CStrExplode client_listent_ip_list(client_listen_ip, ';');
+    for( uint32_t i = 0; i < client_listent_ip_list.GetItemCnt(); ++i)
+    {
+        ret = netlib_listen(client_listent_ip_list.GetItem(i), client_port, client_callback, NULL);
+        if( NETLIB_ERROR == ret )
+        {
+            return ret;
+        }
+    }
+    
+    CStrExplode msg_server_listen_ip_list(msg_server_listen_ip, ';');
+    for (uint32_t i = 0; i < msg_server_listen_ip_list.GetItemCnt(); i++) {
+        ret = netlib_listen(msg_server_listen_ip_list.GetItem(i), msg_server_port, msg_serv_callback, NULL);
+        if (ret == NETLIB_ERROR)
+            return ret;
+    }
+    
+    CStrExplode http_listen_ip_list(http_listen_ip, ';');
+    for (uint32_t i = 0; i < http_listen_ip_list.GetItemCnt(); i++) {
+        ret = netlib_listen(http_listen_ip_list.GetItem(i), http_port, http_callback, NULL);
+        if (ret == NETLIB_ERROR)
+            return ret;
+    }
+    printf("server start listen on:\nFor client %s:%d\nFor MsgServer: %s:%d\nFor http:%s:%d\n",
+           client_listen_ip, client_port, msg_server_listen_ip, msg_server_port, http_listen_ip, http_port);
+    
+    init_login_conn();
+    init_http_conn();
+    
+    printf("now enter the event loop...\n");
+    writePid();
+    
+    netlib_eventloop();
+    
     int i = 0;
     printf("server version:%d", i);
     return 0;
