f0f1a937c2b0f50fe96a25ee19d41e1622a8ee42
hgl888 <hgl868@126.com>
2016-09-23 08:46:11 +0800

    添加MsgConn类定义

--- msg_server/MsgConn.cpp
+++ msg_server/MsgConn.cpp
@@ -1,11 +1,9 @@
 //
-//  RouteServConn.cpp
+//  MsgConn.cpp
 //  Server
 //
 //  Created by mac on 16/9/22.
 //  Copyright © 2016年 mac. All rights reserved.
 //
 
-#include "RouteServConn.h"
 #include "MsgConn.h"
-
--- msg_server/RouteServConn.h
+++ msg_server/RouteServConn.h
@@ -10,5 +10,42 @@
 #define ROUTESERVCONN_H_
 
 #include <stdio.h>
+#include "imconn.h"
+#include "ServInfo.h"
+
+class CRouteServConn : public CImConn
+{
+public:
+    CRouteServConn();
+    virtual ~CRouteServConn();
+    
+    bool IsOpen() { return m_bOpen;}
+    uint64_t GetConnectTime(){ return m_connect_time;}
+    
+    void Connect(const char *server_ip, uint16_t server_port, uint32_t serv_idx);
+    
+    virtual void Close();
+    virtual void OnConfirm();
+    virtual void onClose();
+    virtual void OnTimer(uint64_t curr_tick);
+    
+    virtual void HandlePdu(CImPdu *pPdu);
+    
+private:
+    void _HandleKickUser( CImPdu *pPdu);
+    void _HandleStatusNotify(CImPdu *pPdu);
+    void _HandleMsgREadNotify(CImPdu *pPdu);
+    void _HandleMsgData(CImPdu *pPdu);
+    void _HandleP2Msg(CImPdu *pPdu);
+    void _HandleUsersStatusResponse(CImPdu *pPdu);
+    void _HandlePCLoginStatusNotify(CImPdu *pPdu);
+    void _HandleRemoveSessionNotify(CImPdu *pPdu);
+    void _HandleSignInfoChangedNotify(CImPdu *pPdu);
+    
+private:
+    bool        m_bOpen;
+    uint32_t    m_serv_idx;
+    uint32_t    m_connect_time;
+};
 
 #endif /* RouteServConn_hpp */
